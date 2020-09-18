fab79a167853039c65db62aa2fb1852293e7b99d
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-09-18 11:08:34 +0800

    Add files via upload

---
+++ blog03-ScanIP2Port/test13-test.py
@@ -0,0 +1,12 @@
+import queue
+
+queue = queue.Queue()
+
+for i in range(10):
+    queue.put(i)
+print(queue.empty())
+print(queue.qsize())
+
+#取数据 get依次取出里面的数据
+print(queue.get())
+print(queue.get())
